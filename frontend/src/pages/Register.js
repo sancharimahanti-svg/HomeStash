d0ba12a188ec07e48285f7d8805a1b9b3fe95726
Sanchari <sanchari.mahanti@gmail.com>
2026-08-21 19:13:34 +0530

    added min length in register also for passwrod

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -15,6 +15,10 @@ function Register() {
       toast.error('Please fill all fields');
       return;
     }
+    if (password.length < 6) {
+    toast.error('Password must be at least 6 characters');
+    return;
+  }
 
     try {
       setLoading(true);
